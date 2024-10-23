9fde4ceab159098195cf47adb2c1368ab561d6f5
truuue <noahvernhet@gmail.com>
2024-10-23 12:32:40 +0200

    8. Dashboard routing

--- src/routes/dashboard/Dashboard.js
+++ src/routes/dashboard/Dashboard.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import SideBar from '../../components/navigation/SideBar';
 import './Dashboard.css';
+import HomePage from '../../pages/HomePage';
+import Favorites from '../../pages/Favorites';
+import WhatchLater from '../../pages/WatchLater';
 
-const Dashboard = () => {
+function Dashboard() {
   return (
-    <div className="dashboard">
-      <SideBar />
-      <main className="dashboard-content">
-        {/* Contenu principal du tableau de bord */}
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="dashboard">
+        <SideBar />
+        <main className="dashboard-content">
+          {/* Contenu principal du tableau de bord */}
+          <Routes>
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/favorites" element={<Favorites />} />
+            <Route path="/watchlater" element={<WhatchLater />} />
+            <Route path="*" element={<Navigate to="/home" replace />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
-};
+}
 
 export default Dashboard;
